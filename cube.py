326ca62e2e9aec8617786231e68ea58744c71dea
foucauld <foucauld@meetwonka.com>
2026-06-18 14:00:23 +0200

    cube init

---
+++ cube.py
@@ -0,0 +1,23 @@
+# 0: URF
+# 1: UFL
+# 2: ULB
+# 3: UBR
+# 4: DFR
+# 5: DLF
+# 6: DBL
+# 7: DRB
+
+corner_permutation = [i for i in range(8)]
+corner_orentiation = [0, 1, 2]
+edge_permutation = [i for i in range(12)]
+edge_orientation = [0, 1]
+
+
+class Cube:
+    def __init__(self, corner_permutation, corner_orentiation,edge_permutation, edge_orientation):
+        self.corner_permutation = corner_permutation
+        self.corner_orentiation = corner_orentiation
+        self.edge_permutation = edge_permutation
+        self.edge_orientation = edge_orientation
+
+cube = Cube(corner_permutation, corner_orentiation, edge_permutation, edge_orientation)
