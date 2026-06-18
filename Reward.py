0b0e785f15d5134c0e37ca1f793511e68dcb9f18
gabi0s <g.calmel8@gmail.com>
2026-06-18 16:11:30 +0200

    reward

--- Reward.py
+++ Reward.py
@@ -1,37 +1,45 @@
 from cube import cube_resolved
 
-def calculate_reward():
-
-    reward=correct_position()/63
+def calculate_reward(cube):
+    reward=correct_position(cube)/40 - penalité
     return reward
 
+def STOCKAGE(mvt):
+    L=[]
+    L.append(mvt)
+    return L
+
+
+def penalité(mvt):
+    a=len(STOCKAGE(mvt))-1
+    if STOCKAGE[a]==mvt :
+        return 100
+    else :
+        return 0
 
 def correct_position(cube):
     a=0
     #corner permutation
     for i in range(len(cube.corner_permutation)):
-        print(i)
-        print(cube.corner_permutation[i])
         if i == cube.corner_permutation[i]:
             a+=1
-    print(f'a = {a}')
+    
     #corner orientation
     for i in range(len(cube.corner_orentation)):
        if 0 == cube.corner_orentation[i]:
             a+=1 
-    print(f'a = {a}')
+
     #edge permutation
     for i in range(len(cube.edge_permutation)):
         if i == cube.edge_permutation[i]:
             a+=1
-    print(f'a = {a}')
+    
     #edge orientation
     for i in range(len(cube.edge_orientation)):
        if 0 == cube.edge_orientation[i]:
             a+=1 
-    print(f'a = {a}')
+
     return a
 # 40 correct position
 
-#print(correct_position(cube_with_one_move))
 print(correct_position(cube_resolved))
